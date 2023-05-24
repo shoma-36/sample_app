57a96a1adbc3e967060c85d7836868966cd0117e
shoma <shoma.ochiai.uec@gmail.com>
2023-05-24 13:48:00 +0000

    Use SSL and the Puma web server in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,10 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
+  
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
